1f19d6ef1fabbbaa6b6b28f1561859e335829ac1
Forhad Reza <forhadreza55@gmail.com>
2023-03-26 04:57:23 +0600

    update1

---
+++ android/app/src/main/kotlin/com/example/on_bording/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.on_bording
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,42 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'constants/constants.dart';
-import 'providers/chats_provider.dart';
-import 'providers/models_provider.dart';
-import 'screens/home_page.dart';
-import 'screens/chat_screen.dart';
-import 'screens/splash_screen.dart';
-import 'services/tts_functions.dart';
-import 'screens/image_generator.dart';
-import 'screens/voice_chat.dart';
-import 'widgets/main_drawer.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:on_bording/sign_up_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'onboarding/app_styles.dart';
+import 'onboarding/onboarding_page.dart';
+
+bool? seenOnBoard;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  HttpOverrides.global = MyHttpOverrides();
-  TextToSpeech.initTTS();
-  runApp(const MyApp());
+  // to show status bar
+  SystemChrome.setEnabledSystemUIMode(
+      SystemUiMode.manual,
+    overlays: [SystemUiOverlay.bottom,
+      SystemUiOverlay.top],
+  );
+  SharedPreferences pref = await SharedPreferences.getInstance();
+  seenOnBoard = pref.getBool('ON_BOARDING') ?? true;
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (_) => ModelsProvider(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Habit App',
+      theme: ThemeData(
+        textTheme: GoogleFonts.manropeTextTheme(
+          Theme.of(context).textTheme,
         ),
-        ChangeNotifierProvider(
-          create: (_) => ChatProvider(),
-        ),
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Chat GPT ChatBOT',
-        theme: ThemeData(
-          scaffoldBackgroundColor: scaffoldBackgroundColor,
-          appBarTheme: AppBarTheme(
-            color: cardColor,
-          ),
-          //primarySwatch: Colors.blue,
-        ),
-        initialRoute: SplashScreen.routeName,
-        routes: {
-          SplashScreen.routeName : (_) => const SplashScreen(),
-          MainDrawer.routeName : (_) => const MainDrawer(),
-          HomePage.routeName : (_) => const HomePage(),
-          ImageGenerator.routeName : (_) => const ImageGenerator(),
-          ChatScreen.routeName : (_) => const ChatScreen(),
-          VoiceChatBot.routeName : (_) => const VoiceChatBot(),
-        },
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: kScaffoldBackground,
       ),
+      home: seenOnBoard == true ? const OnBoardingPage() : const SignUpPage(),
     );
-  }
-}
-
-
-
-
-class MyHttpOverrides extends HttpOverrides{
-  @override
-  HttpClient createHttpClient(SecurityContext? context){
-    return super.createHttpClient(context)
-      ..badCertificateCallback = (X509Certificate cert, String host, int port)=> true;
   }
 }
