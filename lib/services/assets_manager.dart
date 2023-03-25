085575231f4d53e4db091d138714acda821d661d
Forhad Reza <forhadreza55@gmail.com>
2023-03-26 05:21:55 +0600

    update1

--- lib/services/assets_manager.dart
+++
@@ -1,6 +0,0 @@
-class AssetsManager {
-  static String imagePath = "images";
-  static String userImage = "$imagePath/person1.png";
-  static String botImage = "$imagePath/chat_logo.png";
-  static String openaiLogo = "$imagePath/openai_logo.jpg";
-}
