cb4a36e0322a4d5344cda9ee9af56cf5eb7dd416
Forhad Reza <forhadreza55@gmail.com>
2023-03-26 05:21:10 +0600

    update1

--- android/app/src/main/kotlin/com/example/on_bording/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.on_bording
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/constants/api_consts.dart
+++
@@ -1,2 +0,0 @@
-//String BASE_URL = "https://your api url";
-//String API_KEY = "your api key";
